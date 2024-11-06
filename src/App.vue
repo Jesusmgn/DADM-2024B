de476488c9206afcf5b1633e74ebfb824028f99c
Jesusmgn <159492890+Jesusmgn@users.noreply.github.com>
2024-11-06 01:02:02 +0000

    ⭐Modify readme App.vue and plan-item.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import planItem from './components/plan-item.vue';
+</script>
 
 <template>
   <header>
@@ -10,62 +12,13 @@
 
   <div class="content">
     <h1 class="title">Coffee Plans</h1>
-
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
-
     <div class="plans">
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Soltero
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Curioso
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El adicto
-          </span>
-        </div>
-      </div>
+      <plan-item>El Soltero</plan-item>
+      <plan-item>El Curioso</plan-item>
+      <plan-item>El Adicto</plan-item>
     </div>
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
