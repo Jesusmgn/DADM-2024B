792262bc753d9a48bfa639dea32e919f225d5d02
jesus <jesusmgn1001@gmail.com>
2024-11-20 11:46:16 -0600

    ⭐S03 S05 Eventos

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,26 +1,32 @@
 <script setup>
 import PlanPickerItem from './PlanPickerItem.vue';
 import { ref } from 'vue';
-//Creando una Referencia reactiva 
-// para almacenar el plan seleccionado
+
+// creando una referencia reactiva para el plan seleccionado
 const selectedPlan = ref(null);
+
+// lista de planes
 const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
+
+// función que permite almacenar el plan seleccionado
 const selectPlan = (name) => {
-  // Almacenar el plan seleccionado
+  // almacenar el plan seleccionado
   selectedPlan.value = name;
-}
+};
 </script>
 
 <template>
   <div class="plans">
     <PlanPickerItem
-     v-for="plan in plans" 
-     :key="plan" 
-     :name="plan"
-     @select="selectPlan"
-     :select-plan="selectPlan"></PlanPickerItem>
+      v-for="plan in plans" 
+      :key="plan" 
+      :name="plan"
+      :selectPlan="selectedPlan"
+      @select="selectPlan" 
+    />
   </div>
-  <p> {{ selectedPlan }} </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Estilos del componente padre (si son necesarios) */
+</style>
--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,37 +1,37 @@
 <template>
-  <div  @click="selectPlan" class="plan">
+  <div @click="selectPlan" class="plan" :class="{ selected: isSelected }">
     <div class="description">
+      <!-- Mostrar el título y el emoji si el plan está seleccionado -->
       <span class="title"> {{ name }} {{ isSelected ? '😁' : '' }}</span>
     </div>
   </div>
 </template>
 
 <script setup>
-// Importamos la funcion para crear 
-// propiedad computada
+// importamos la función para crear una propiedad computada
 import { computed } from 'vue';
-// Usando un macro para
-// definir las props
-//defineProps(['name','planIcon']);
+
+// Usando un macro para definir las props
 const props = defineProps({
   name: {
-    tyoe: String,
+    type: String,
     required: true
   },
+  // Propiedad que indica el plan actualmente seleccionado
   selectPlan: String
 });
+
 // Definiendo un evento
-const emit = defineEmits (['select']);
-// Función que permite cambiar el estado de la variable 
-// selected 
+const emit = defineEmits(['select']);
+
+// Función que permite cambiar el estado del plan seleccionado
 const selectPlan = () => {
-  // Emitir evento de que el 
-  //plan ha sido seleccionado
+  // Emitir el evento para indicar que el plan ha sido seleccionado
   emit('select', props.name);
 };
-// Creando una propiedad computada
-// para saber si el plan ha sido seleccionado
-const isSelected = computed (() => {
+
+// creamos una propiedad computada para saber si el plan ha sido seleccionado
+const isSelected = computed(() => {
   return props.name === props.selectPlan;
 });
 </script>
